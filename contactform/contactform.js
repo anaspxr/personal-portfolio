fbcfd124b10bb729e5fa726ef4609a10a45ef248
ANAS <anaspappadan@gmail.com>
2024-05-28 18:25:18 +0530

    first commit

---
+++ contactform/contactform.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  const serviceID = "service_m3mp1rk";
+  const templateID = "template_bdspw4t";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      document.getElementById("subject").value = "";
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
